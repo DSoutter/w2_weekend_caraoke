a221c7d4f61b543043fee9f67a7c10e2c134f245
DSoutter <duncan.soutter@gmail.com>
2021-07-30 16:04:52 +0100

    Room name passing

--- tests/room_test.py
+++ tests/room_test.py
@@ -11,3 +11,5 @@ class TestRoom(unittest.TestCase):
             self.room2 = Room("Christmas", 5, 30, 600)
             self.room3 = Room("Disco", 20, 75, 2000)
 
+        def test_room1_has_genre(self):
+            self.assertEqual("Abba", self.room1.genre)
